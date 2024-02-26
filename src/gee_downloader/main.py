ef1ce3c59a5afd46766461e7afa1c1e652c723c7
Jancauskas <vytautas.jancauskas@dlr.de>
2024-02-26 14:24:17 +0100

    added a test for read_polygon

--- src/gee_downloader/main.py
+++ src/gee_downloader/main.py
@@ -1,4 +1,23 @@
 import geedim
+import json
+
+def read_polygon(geojson):
+    """Reads a polygon identifying an area of interest from a GeoJSON file.
+
+    Parameters
+    ----------
+    geojson: str
+        A path to a valid GeoJSON file with a single polygon identifying an 
+        area of interest.
+    
+    Returns
+    -------
+    dict
+        A dictionary with the polygon.
+    """
+    with open(geojson, 'r') as fd:
+        data = json.load(fd)
+    return data["features"][0]["geometry"]
 
 if __name__ == '__main__':
     pass
